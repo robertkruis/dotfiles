57d9f8e88cc13d080f57ff09ff3b2a2a95dfd0fd
Robert Kruis <azforeversupporter@gmail.com>
2023-08-27 02:17:49 +0200

    add wezterm

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,26 @@
+local wezterm = require("wezterm")
+
+local scoop_path = os.getenv("SCOOP")
+
+return {
+	default_prog = {
+		string.format("%s\\apps\\git\\current\\bin\\bash.exe", scoop_path),
+		"--login",
+		"-i",
+		"-l",
+	},
+	color_scheme = "Gruvbox dark, hard (base16)",
+	font_dirs = { "fonts" },
+	font_size = 10.5,
+	font = wezterm.font_with_fallback({
+		{
+			family = "CommitMono",
+			harfbuzz_features = { "ss01", "ss06", "ss10", "zero", }
+		},
+		{ family = "Symbols NFM" },
+	}),
+	line_height = 1.0,
+	cell_width = 1.0,
+	cursor_thickness = "100%",
+	default_cursor_style = "SteadyUnderline",
+}
