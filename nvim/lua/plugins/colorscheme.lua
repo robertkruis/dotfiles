c0916ee96427574d44e555bafc4de2d26721a68a
Robert Kruis <azforeversupporter@gmail.com>
2023-09-30 19:31:13 +0200

    [nvim] use everforest as default theme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -5,19 +5,15 @@ return {
   -- add custom colorschemes
   { "EdenEast/nightfox.nvim" },
   { "rose-pine/neovim", name = "rose-pine" },
-  -- {
-  --   "sainnhe/everforest",
-  --   config = function()
-  --     vim.g.everforest_background = "soft"
-  --     vim.cmd.colorscheme("everforest")
-  --   end,
-  -- },
+  {
+    "sainnhe/everforest",
+  },
 
   -- change the colorscheme
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "nordfox",
+      colorscheme = "everforest",
     },
   },
 }
