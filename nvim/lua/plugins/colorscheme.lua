c66b40136806db8ccff3e4b5ce9add0ac8a25b52
Robert Kruis <azforeversupporter@gmail.com>
2024-04-16 21:52:42 +0200

    [nvim] change colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,30 +1,28 @@
 return {
-  -- disable default colorschemes
-  { "folke/tokyonight.nvim", enabled = false },
-
-  -- add custom colorschemes
-  { "EdenEast/nightfox.nvim" },
-  { "rose-pine/neovim", name = "rose-pine" },
   {
-    "sainnhe/everforest",
-  },
-  {
-    "sainnhe/gruvbox-material",
-    lazy = false,
+    "rose-pine/neovim",
+    name = "rose-pine",
     priority = 1000,
-    init = function()
-      vim.g.gruvbox_material_background = "soft"
-      vim.g.gruvbox_material_enable_italic = 1
-      vim.g.gruvbox_material_show_eob = 0
-	  vim.g.gruvbox_material_float_style = "dim"
-    end,
+    lazy = false,
+    opts = {
+      dark_variant = "moon",
+      enable = {
+        legacy_highlights = false,
+      },
+      styles = {
+        italic = false,
+      },
+      highlight_groups = {
+        Comment = { italic = true },
+        Keyword = { italic = true },
+      },
+    },
   },
-
   -- change the colorscheme
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox-material",
+      colorscheme = "rose-pine",
     },
   },
 }
