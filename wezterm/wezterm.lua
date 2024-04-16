58bbbeeee15ee0a77be5ab4be228f36117932dcc
Robert Kruis <azforeversupporter@gmail.com>
2024-04-16 21:51:44 +0200

    [wezterm] change font, add theme and ligature switch

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,19 +1,49 @@
 local wezterm = require("wezterm")
 
+local color_scheme_dark = "Edge Dark (base16)"
+local color_scheme_light = "Edge Light (base16)"
+
+wezterm.on("toggle-theme", function(window, _)
+	local overrides = window:get_config_overrides() or {}
+	if not overrides.color_scheme then
+		overrides.color_scheme = color_scheme_light
+	else
+		if overrides.color_scheme == color_scheme_dark then
+			overrides.color_scheme = color_scheme_light
+		else
+			overrides.color_scheme = color_scheme_dark
+		end
+	end
+
+	window:set_config_overrides(overrides)
+end)
+
+wezterm.on("toggle-ligatures", function(window, _)
+	local overrides = window:get_config_overrides() or {}
+	if not overrides.harfbuzz_features then
+		overrides.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
+	else
+		overrides.harfbuzz_features = nil
+	end
+
+	window:set_config_overrides(overrides)
+end)
+
 return {
 	default_prog = { "powershell.exe", "-NoLogo" },
-	color_scheme = "Gruvbox dark, hard (base16)",
+	color_scheme = color_scheme_dark,
 	font_dirs = { "fonts" },
 	font_size = 11.25,
 	font = wezterm.font_with_fallback({
-		-- { family = "Monaspace Krypton" },
-		{ family = "Maple Mono" },
+		{ family = "Intel One Mono" },
 		{ family = "Symbols NFM" },
 	}),
 	line_height = 1.0,
 	cell_width = 1.0,
 	cursor_thickness = "100%",
 	default_cursor_style = "SteadyUnderline",
+	freetype_load_target = "Light",
+	freetype_render_target = "HorizontalLcd",
 
 	-- Fix CTRL + SPACE issue for Neovim/Git Bash (https://github.com/wez/wezterm/issues/405)
 	keys = {
@@ -24,6 +54,16 @@ return {
 				key = " ",
 				mods = "CTRL",
 			}),
+		},
+		{
+			key = "t",
+			mods = "ALT",
+			action = wezterm.action.EmitEvent("toggle-theme"),
+		},
+		{
+			key = "l",
+			mods = "ALT",
+			action = wezterm.action.EmitEvent("toggle-ligatures"),
 		},
 	},
 }
